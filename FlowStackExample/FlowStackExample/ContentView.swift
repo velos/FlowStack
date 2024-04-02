c7498082bfc145235cb85fd119e46dcf3b620bfe
Charlie Hieger <charlie@velosmobile.com>
2024-04-01 18:10:40 -0700

    Example project: Use grid for regular horizontal size class

--- FlowStackExample/FlowStackExample/ContentView.swift
+++ FlowStackExample/FlowStackExample/ContentView.swift
@@ -9,22 +9,37 @@ import SwiftUI
 import FlowStack
 
 struct ContentView: View {
+    @Environment(\.horizontalSizeClass) private var horizontalSizeClass
+
     let cornerRadius: CGFloat = 24
 
     var body: some View {
         FlowStack {
             ScrollView {
-                LazyVStack(alignment: .center, spacing: 24, pinnedViews: [], content: {
-                    ForEach(Product.allProducts) { product in
-                        FlowLink(value: product, configuration: .init(cornerRadius: cornerRadius)) {
-                            ProductRow(product: product, cornerRadius: cornerRadius)
-                        }
+                Group {
+                    if horizontalSizeClass == .compact {
+                        LazyVStack(alignment: .center, spacing: 24, pinnedViews: [], content: {
+                            content
+                        })
+
+                    } else {
+                        LazyVGrid(columns: [GridItem(.flexible(), spacing: 16), GridItem(.flexible())], alignment: .center, spacing: 16, content: {
+                            content
+                        })
                     }
-                })
+                }
                 .padding(.horizontal)
             }
             .flowDestination(for: Product.self) { product in
                 ProductDetails(product: product)
+            }
+        }
+    }
+
+    var content: some View {
+        ForEach(Product.allProducts) { product in
+            FlowLink(value: product, configuration: .init(cornerRadius: cornerRadius)) {
+                ProductRow(product: product, cornerRadius: cornerRadius)
             }
         }
     }
